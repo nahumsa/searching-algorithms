0614e2a6f2dc2e7ff1de1bf0c589c978a8a698a8
nahumsa <nahumsaa@gmail.com>
2020-09-07 10:21:46 -0300

    Added benchmark

--- linear/linear_test.go
+++ linear/linear_test.go
@@ -4,27 +4,7 @@ import (
 	"fmt"
 	"math/rand"
 	"testing"
-	"time"
 )
-
-// intList creates a ordered list with n elements
-func intList(n int) []int {
-	list := make([]int, n)
-	for i := 0; i < n; i++ {
-		list[i] += i
-	}
-	return list
-}
-
-// shuffle Shuffles a list using Fisher-Yates shuffle
-func shuffle(list []int) {
-	rand.Seed(time.Now().UnixNano())
-
-	for i := len(list) - 1; i > 0; i-- { // Fisher–Yates shuffle
-		j := rand.Intn(i + 1)
-		list[i], list[j] = list[j], list[i]
-	}
-}
 
 func TestLinear(t *testing.T) {
 	want := []bool{true, false, true, false, true}
@@ -51,18 +31,13 @@ func TestLinear(t *testing.T) {
 }
 
 func BenchmarkLinear(b *testing.B) {
-	for _, size := range []int{
-		100, 200, 400, 800, 1600, 3200,
-	} {
-		fmt.Println("Size:", size)
-		for n := 0; n < b.N; n++ {
-			b.StopTimer()
-
-			list := rand.Perm(size)
-			search := rand.Intn(size)
-
-			b.StartTimer()
-			_ = Find(search, list)
-		}
+	for _, size := range []int{100, 200, 400, 800, 1600} {
+		b.Run(fmt.Sprintf("%v", size), func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				list := rand.Perm(size)
+				search := rand.Intn(size)
+				Find(search, list)
+			}
+		})
 	}
 }
